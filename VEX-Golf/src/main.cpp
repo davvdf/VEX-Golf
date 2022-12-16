55f60c322e7797f74bb181d0a956d549a7a2d210
dennisrage111 <dennisslan25@gmail.com>
2022-12-16 12:04:05 -0500

    upload past code

--- VEX-Golf/src/main.cpp
+++ VEX-Golf/src/main.cpp
@@ -23,5 +23,17 @@ using namespace vex;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+  armMotor.spinFor(reverse, 193, degrees);
+ while (true) {
+   if ( Vision9.takeSnapshot(Vision9__BOB) || Vision9.takeSnapshot(Vision9__BOBBY)){
+     if (Vision9.largestObject.centerY > 105.5){
+        armMotor.stop();
+        clawMotor.spin(reverse);
+        wait(3,seconds);
+        armMotor.spinFor(reverse, 360, degrees);
+     }
+    }
+   wait(25, msec);
+ }
   
 }
