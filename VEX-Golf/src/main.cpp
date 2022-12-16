5f84851ac05ead08cc4409b678230ec502dd7107
davvd123 <david88gao@gmail.com>
2022-12-16 11:56:30 -0500

    cool
    Co-Authored-By: gajans12345 <90165455+gajans12345@users.noreply.github.com>
    Co-Authored-By: dennisrage111 <120740927+dennisrage111@users.noreply.github.com>

---
+++ VEX-Golf/src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\david                                            */
+/*    Created:      Fri Dec 16 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    11, 20          
+// armMotor             motor         19              
+// clawMotor            motor         1               
+// Vision9              vision        9               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
---
+++ VEX-Golf/src/robot-config.cpp
@@ -0,0 +1,31 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT11, ratio18_1, false);
+motor RightDriveSmart = motor(PORT20, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+motor armMotor = motor(PORT19, ratio36_1, true);
+motor clawMotor = motor(PORT1, ratio18_1, true);
+/*vex-vision-config:begin*/
+vision Vision9 = vision (PORT9, 50);
+/*vex-vision-config:end*/
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
