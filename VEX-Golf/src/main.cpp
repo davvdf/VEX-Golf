f474798d8ce912afe95d82027e5234165a74fe9a
davvd123 <david88gao@gmail.com>
2023-01-13 16:38:37 -0500

    fixed things

--- VEX-Golf/src/main.cpp
+++ VEX-Golf/src/main.cpp
@@ -16,6 +16,65 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
+// armMotor             motor         19              
+// clawMotor            motor         1               
+// Vision6              vision        6               
+// rightDist            distance      18              
+// leftDist             distance      12              
+// LineTrackerA         line          A               
+// LineTrackerB         line          B               
+// LineTrackerC         line          C               
+// LineTrackerD         line          D               
+// LineTrackerE         line          E               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// armMotor             motor         19              
+// clawMotor            motor         1               
+// Vision6              vision        6               
+// rightDist            distance      18              
+// leftDist             distance      12              
+// LineTrackerA         line          A               
+// LineTrackerB         line          B               
+// LineTrackerC         line          C               
+// LineTrackerD         line          D               
+// LineTrackerE         line          E               
+// Drivetrain           drivetrain    11, 20, 2       
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// armMotor             motor         19              
+// clawMotor            motor         1               
+// Vision6              vision        6               
+// rightDist            distance      18              
+// leftDist             distance      12              
+// LineTrackerA         line          A               
+// LineTrackerB         line          B               
+// LineTrackerC         line          C               
+// LineTrackerD         line          D               
+// LineTrackerE         line          E               
+// Drivetrain           drivetrain    11, 20, 2       
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// armMotor             motor         19              
+// clawMotor            motor         1               
+// Vision6              vision        6               
+// rightDist            distance      18              
+// leftDist             distance      12              
+// LineTrackerA         line          A               
+// LineTrackerB         line          B               
+// LineTrackerC         line          C               
+// LineTrackerD         line          D               
+// LineTrackerE         line          E               
+// Drivetrain           drivetrain    11, 20          
+// ---- END VEXCODE CONFIGURED DEVICES ----
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
 // Drivetrain           drivetrain    11, 20          
 // armMotor             motor         19              
 // clawMotor            motor         1               
@@ -181,37 +240,52 @@ using namespace vex;
 
 int thresholdVal = 50;
 bool hasBall;
+bool turningState;
+bool afterTurn;
 
 void driveSense(){
   if (LineTrackerB.reflectivity()>thresholdVal){
+    afterTurn = false;
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("driving");
     Drivetrain.drive(reverse);
-  } else if (LineTrackerA.reflectivity()>thresholdVal){
-    Drivetrain.turnFor(right, 1, degrees);
-    wait(5, msec);
-  } else if (LineTrackerC.reflectivity()>thresholdVal){
+  } else if (LineTrackerA.reflectivity()<thresholdVal){
     Drivetrain.turnFor(left, 1, degrees);
-    wait(5, msec);
-  } else if (LineTrackerE.reflectivity()>thresholdVal){
-    Drivetrain.driveFor(reverse, 210, mm);
-    Drivetrain.turnFor(left, 90, degrees);
-  } else if (LineTrackerD.reflectivity()>thresholdVal){
-    Drivetrain.driveFor(reverse, 210, mm);
+  } else if (LineTrackerC.reflectivity()<thresholdVal){
+    Drivetrain.turnFor(right, 1, degrees);
+  } else if (LineTrackerE.reflectivity()<thresholdVal){
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("right");
+    Drivetrain.driveFor(reverse, 220, mm);
     Drivetrain.turnFor(right, 90, degrees);
-  } 
+    afterTurn = true;
+  } else if (LineTrackerD.reflectivity()<thresholdVal){
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("left");
+    Drivetrain.driveFor(reverse, 220, mm);
+    Drivetrain.turnFor(left, 90, degrees);
+    afterTurn = true;
+  }
 }
 
 void getBall(){
   Drivetrain.drive(forward);
   if (Vision6.takeSnapshot(Vision6__BALL)){
-    Drivetrain.stop();
-    hasBall = true;
-    if (Vision6.largestObject.centerY > 100){
+    if (Vision6.largestObject.centerY > 90){
+      Brain.Screen.clearScreen();
+      Brain.Screen.setCursor(1,1);
+      Brain.Screen.print("found ball");
+      Drivetrain.stop();
       clawMotor.spin(forward);
       armMotor.spinToPosition(90, degrees);
       armMotor.stop();        
-      wait(2,seconds);
+      wait(3,seconds);
       clawMotor.stop();
       armMotor.spinToPosition(90, degrees);
+      hasBall = true;
     }
   }
 }
@@ -223,20 +297,24 @@ void senseT(){
     wait(3, sec);
     armMotor.spinToPosition(190, degrees);
     hasBall = false;
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("Dumping ball");
     if (rightDist.objectDistance(mm) > 250){
       Drivetrain.turnFor(right , 90, degrees);
     }
     if (leftDist.objectDistance(mm) > 250){
       Drivetrain.turnFor(left, 90, degrees);
     }
-    Drivetrain.driveFor(forward, 500, mm);
-    //Drivetrain.turnFor(right, 90, degrees);
-    
-    if ( leftDist.objectDistance(mm) < rightDist.objectDistance(mm)){
-      Drivetrain.turnFor(right,90,degrees);
-    }
-    else {
+    Drivetrain.driveFor(forward, 600, mm);
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("Sensing left/right");
+    if (leftDist.objectDistance(mm) < rightDist.objectDistance(mm)){
       Drivetrain.turnFor(left,90,degrees);
+    }
+    if (leftDist.objectDistance(mm) > rightDist.objectDistance(mm)) {
+      Drivetrain.turnFor(right,94,degrees);
     }
   }
 }
@@ -246,7 +324,7 @@ int main() {
   vexcodeInit();
   armMotor.setVelocity(50, percent);
   armMotor.spinToPosition(190, degrees);
-  Drivetrain.setDriveVelocity(25, percent);
+  Drivetrain.setDriveVelocity(18, percent);
   Drivetrain.setTurnVelocity(25, percent);
   hasBall = false;
   while (true) {
